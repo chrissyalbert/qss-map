4b2f61dc011e5edf11635573114fb601487e991e
chrissyalbert <chrissy_albert@yahoo.com>
2021-02-23 11:44:14 -0700

    set up React, initial variables

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import {Map} from './Components/Map'
 import './App.css';
 
+let usersLocations = [ 
+  {id: 0, lat: 42.360081, long: -71.058884},
+  {id: 1, lat: 41.878113, long: -87.629799},
+  {id: 2, lat: 33.748997, long: -84.387985},
+  {id: 3, lat: 39.739235, long: -104.990250},
+  {id: 4, lat: 33.448376, long: -112.074036},
+  {id: 5, lat: 58.301945, long: -134.419724},
+  {id: 6, lat: 47.606209, long: -122.332069},
+  {id: 7, lat: 21.306944, long: -157.858337}
+ ]
+
+function addLoc(id) {
+  if (navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(function(position) {
+      let latitude = position.coords.latitude;
+      let longitude = position.coords.longitude;
+      usersLocations.push({id, latitude, longitude});
+    });
+  }
+}
+
+function deleteLoc(value) {
+  let i = usersLocations.length;
+  while(i--){
+    if(usersLocations[i] && (arguments.length > 2 && usersLocations[i][id] === value)) {
+      usersLocations.splice(i,1);    
+      }
+  }
+}
+
+function getLocation () {
+  if (navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(function(position) {
+      // let latitude = position.coords.latitude;
+      // let longitude = position.coords.longitude;
+      usersLocations.push(position.coords);
+    });
+  }
+}
+
 function App() {
+  const coordinates = getLocation();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map />
     </div>
   );
 }
 
 export default App;
+
+/*
+var removeByAttr = function(arr, attr, value){
+  var i = arr.length;
+  while(i--){
+     if( arr[i] 
+         && arr[i].hasOwnProperty(attr) 
+         && (arguments.length > 2 && arr[i][attr] === value ) ){ 
+
+         arr.splice(i,1);
+
+     }
+  }
+  return arr;
+}
+*/
---
+++ src/Components/Map.js
@@ -0,0 +1,16 @@
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+export function Map() {
+
+  return (
+    <Container fluid >
+      <Row>
+        <Col md={8} >
+        
+        </Col>
+      </Row>
+    </Container>
+  )
+}
